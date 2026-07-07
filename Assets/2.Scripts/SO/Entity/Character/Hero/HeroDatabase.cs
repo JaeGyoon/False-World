bcb2c945aaa3113e2c6c3c634234896494b60122
jgPark_Home <rkdnjsxpzm@gmail.com>
2026-07-07 19:07:30 +0900

    feat : HeroDatabase 구현

---
+++ Assets/2.Scripts/SO/Entity/Character/Hero/HeroDatabase.cs
@@ -0,0 +1,53 @@
+﻿using FalseWorld;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "HeroDatabase", menuName = "Scriptable Objects/HeroDatabase")]
+public class HeroDatabase : ScriptableObject
+{
+    [SerializeField] private List<HeroDataSO> heros = new List<HeroDataSO>();
+
+    private Dictionary<EntityID, HeroDataSO> heroDict;
+
+    public void Initialize()
+    {
+        heroDict = new Dictionary<EntityID, HeroDataSO>();
+
+        foreach (HeroDataSO so in heros)
+        {
+            if ( so == null)
+            {
+                continue;
+            }
+
+            if ( heroDict.ContainsKey(so.ID))
+            {
+                Debug.Log("중복된 ID가 있습니다.");
+                continue;
+            }
+
+            heroDict.Add(so.ID, so);
+        }
+    }
+
+    public HeroDataSO Get(EntityID id)
+    {
+        if ( heroDict == null )
+        {
+            Initialize();
+        }
+
+        if (heroDict.TryGetValue(id, out HeroDataSO data))
+        {
+            return data;
+        }
+        else
+        {
+            Debug.Log($"해당 ID가 없습니다. {id}");
+            return null;
+        }
+    }
+
+    public IReadOnlyList<HeroDataSO> AllHeros => heros;
+
+}
